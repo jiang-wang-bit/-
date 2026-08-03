d7c147cfe2591797f991fb8d17b9a20c24978ed9
jiangwang <jiangwang0205@qq.com>
2026-08-03 16:42:07 +0800

    feat:用户登录退出

--- src/layouts/FrontLayout/components/FrontHeader/index.tsx
+++ src/layouts/FrontLayout/components/FrontHeader/index.tsx
@@ -1,8 +1,15 @@
-import { Menu } from "antd"
+import { Menu,Avatar,Dropdown} from "antd"
 import { useNavigate } from "react-router-dom"
+import { UserOutlined } from "@ant-design/icons"
+import { useDispatch,useSelector } from "react-redux"
+import type { RootState } from "../../../../store"
+import { logout } from "../../../../store/modules/auth"
 import "./index.scss"
 export default function FrontHeader(){
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+  // 获取用户信息
+  const userInfo = useSelector((state:RootState)=>state.user.userInfo)
   const items = [
     {
       key:"/",
@@ -20,15 +27,40 @@ export default function FrontHeader(){
       key:"/user/favorite",
       label:"我的收藏"
     },
+  ]
+  // 用户菜单
+  const useMenu = [
     {
-      key:"/user",
-      label:"用户中心"
+      key:"profile",
+      label:"个人中心"
+    },
+    {
+      key:"logout",
+      label:"退出登录"
     }
   ]
+
+  const handleUserClick = ({key}:any)=>{
+    if(key==="profile"){
+      navigate("/user")
+    }
+    if(key==="logout"){
+      dispatch(logout())
+      navigate("/")
+    }
+  }
   return(
     <header className="front-header">
       <div className="logo">my logo</div>
       <Menu mode="horizontal" items={items} onClick={({key})=>navigate(key)}/>
+      <div className="header-user">
+        <Dropdown menu={{items:useMenu,onClick:handleUserClick}}>
+           <div className="user-info">
+            <Avatar size={35} src={userInfo?.avatar} icon={<UserOutlined/>}/>
+            <span>{userInfo?.username||"游客"}</span>
+           </div>
+        </Dropdown>
+      </div>
     </header>
   )
 }
