18153ab1162631d718b099c9ad1f6e6b9a8d81cc
jiangwang <jiangwang0205@qq.com>
2026-08-05 15:33:30 +0800

    头像上传

--- src/pages/Front/UserCenter/Profile/index.tsx
+++ src/pages/Front/UserCenter/Profile/index.tsx
@@ -1,8 +1,10 @@
-import {Card,Form,Input,Button,Avatar,message} from "antd"
-import {UserOutlined} from "@ant-design/icons"
+import {Card,Form,Input,Button,Avatar,message,Upload} from "antd"
+import {UserOutlined,UploadOutlined} from "@ant-design/icons"
 import { useSelector,useDispatch } from "react-redux"
-import { updateUserInfo } from "../../../../store/modules/auth"
+import { updateUserInfo} from "../../../../store/modules/auth"
 import type { RootState } from "../../../../store"
+import ImgCrop from "antd-img-crop"
+import imageCompression from "browser-image-compression"
 import "./index.scss"
 export default function Profile() {
   const dispatch = useDispatch()
@@ -17,7 +19,42 @@ export default function Profile() {
   return (
      <Card title="个人资料" className="profile-card">
       <div className="avatar-box">
-        <Avatar size={80} src={userInfo?.avatar} icon={<UserOutlined/>}/>
+        <ImgCrop rotationSlider showGrid aspect={1}>
+        <Upload showUploadList={false} beforeUpload={async(file)=>{
+          // 文件类型限制
+          const isImage = file.type==="image/jpeg"||file.type==="image/png"||file.type==="image/webp"
+          if(!isImage){
+            message.error("只能上传JPG,PNG,WEBP格式图片")
+            return Upload.LIST_IGNORE
+          }
+          // 文件大小限制
+          const isLt2M = file.size/1024/1024<2
+          if(!isLt2M){
+            message.error("头像大小不能超过2MB")
+            return Upload.LIST_IGNORE
+          }
+          // 压缩
+          const options = {
+            maxSizeMB:0.2,
+            maxWidthOrHeight:300
+          }
+          const compressedFile =
+          await imageCompression(
+          file,
+          options
+          )
+          const reader = new FileReader()
+          reader.onload=()=>{
+           const avatarBase64 = reader.result as string
+            dispatch(updateUserInfo({avatar:avatarBase64}))
+          message.success("头像更新成功")
+          }
+          reader.readAsDataURL(compressedFile)
+          return false
+        }}>
+           <Avatar size={80} src={userInfo?.avatar} icon={<UserOutlined/>} style={{cursor:"pointer"}}/>
+        </Upload>
+        </ImgCrop>
       </div>
 
       <Form form={form} layout="vertical" initialValues={{
