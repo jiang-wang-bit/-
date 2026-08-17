40183cfae7c0a873303cb34e92bec128a65e1ef9
jiangwang <jiangwang02-5@qq.com>
2026-08-17 14:47:06 +0800

    feat:优化dashboard

--- app/routers/admin_dashboard.py
+++ app/routers/admin_dashboard.py
@@ -1,6 +1,7 @@
 from app.database import get_db
 from app.dependencies.auth import require_admin
 from fastapi import APIRouter,Depends
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.models.article import Article
@@ -20,8 +21,70 @@ def get_dashborad(db:Session=Depends(get_db),admin=Depends(require_admin)):
 
   category_count = db.query(Category).count()
 
+  publish_article_count = db.query(Article).filter(Article.status=="published").count()
+
+  draft_article_count = db.query(Article).filter(Article.status=="draft").count()
+
+  # 文章发布趋势
+
+  publish_trend = (
+    db.query(func.date(Article.create_time).label("date"),
+             func.count(Article.id).label("count")).filter(
+               Article.status=="published"
+             ).group_by(
+               func.date(Article.create_time)
+             ).order_by(
+               func.date(Article.create_time)
+             ).all()
+  )
+
+  # 最近发布文章
+  recent_articles = (
+    db.query(Article).filter(Article.status=="published").order_by(Article.create_time.desc()).limit(5).all()
+  )
+
+  # 分类文章数量
+  category_trend = (
+    db.query(Category.name,func.count(Article.id)).outerjoin(
+      Article,
+        (Category.id == Article.category_id)
+        &
+        (Article.status=="published")
+    ).group_by(
+      Category.name
+    ).all()
+  )
+
   return{
     "userCount":user_count,
     "articleCount" :article_count,
-    "categoryCount":category_count
+    "categoryCount":category_count,
+    "publishArticleCount":publish_article_count,
+    "draftArticleCount":draft_article_count,
+    # 折线图
+      "publishTrend":[
+          {
+              "date":str(item.date),
+              "count":item.count
+          }
+          for item in publish_trend
+      ],
+      # 最近文章
+      "recentArticles":[
+          {
+              "id":article.id,
+              "title":article.title,
+              "author":article.author,
+              "createTime":article.create_time
+          }
+          for article in recent_articles
+      ],
+      # 饼图
+      "categoryTrend":[
+          {
+              "name":item[0],
+              "value":item[1]
+          }
+          for item in category_trend
+      ]
   }
